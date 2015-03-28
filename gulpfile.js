ef20a1cfd0a4e6534ed0fbeee3ce754e4fc33541
jquense <monastic.panic@gmail.com>
2015-03-28 13:18:07 -0400

    release v0.4.1

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,13 @@ var gulp    = require('gulp')
   , configs = require('./webpack.configs')
   , babelTransform = require('gulp-babel-helpers')
   , webpack = require('webpack')
-  , prompt  = require('prompt')
-  , fs      = require('fs')
-  , spawn   = require('child_process').spawn
+  , release   = require('rf-release')
   , WebpackDevServer = require("webpack-dev-server");
 
 
 gulp.task('clean', function(cb){
   rimraf('./lib', cb);
 })
-
-
 
 gulp.task('transpile', ['clean'], function(){
 
@@ -47,89 +43,7 @@ gulp.task('dev', function() {
 
 gulp.task('release', ['clean', 'transpile'])
 
-gulp.task('publish', function(cb){
-  var schema = {
-    properties: {
-      version: {
-        description: 'version? (old is '+ require('./package.json').version +')',
-        pattern: /^[0-9]\.[0-9]+\.[0-9](-.+)?/,
-        message: 'Must be a valid semver string i.e. 1.0.2, 2.3.0-beta.1',
-        required: true
-      }
-    }
-  };
-
-  prompt.start();
-
-  prompt.get(schema, function(err, result) {
-    if (err) throw err;
-
-    var rawVersion = result.version
-      , version = 'v' + rawVersion
-      , npm   = require('./package.json')
-      , bower = require('./bower.json')
-      , oldVersion = npm.version;
-
-    updateVersion(rawVersion)
-
-    run('git co master && gulp release', function(){
-      run('npm test', function() {
-        run('git cm "release '+ version +'"', function () {
-          run('git tag -am ' + rawVersion + ' ' + version, function() {
-            run('git push origin master --follow-tags ', function(){
-              run('npm publish', cb);
-            })
-          })
-        })
-      })
-    })
-
-    function updateVersion(ver){
-      bower.version = npm.version = ver
-      fs.writeFileSync('./package.json', JSON.stringify(npm, null, 2));
-      fs.writeFileSync('./bower.json', JSON.stringify(bower, null, 2));
-    }
-
-    function run(command, callback) {
-      console.log('spawn: ' + command);
-      sp(command, function(error) {
-        if (error) {
-          updateVersion(oldVersion)
-          throw new Error(error)
-        } 
-        if (callback) callback();
-      });
-    }
-  });
+gulp.task('publish', ['release'], function(cb){
+  release(cb)
 })
 
-
-function sp(cmd, cb){
-  var child = spawn('cmd.exe', ['/s', '/c', '"' + cmd + '"'], { windowsVerbatimArguments: true })
-    , stderr, err, exited;
-
-  child.stdout.on('data', function (data) {
-    process.stdout.write(data.toString('utf8'))
-  });
-
-  child.stderr.on('data', function (data) {
-    stderr += data.toString('utf8')
-    process.stdout.write(data.toString('utf8'))
-  });
-
-  child.on('close', onExit)
-  child.on('error', function (error) {
-    err = error
-    child.stdout.destroy();
-    child.stderr.destroy();
-    onExit()
-  });
-
-  function onExit(code) {
-    if (exited) return;
-    exited = true;
-    if ( code === 0 ) cb(null)
-    else if (!err) err = new Error('Command failed: ' + cmd + '\n' + stderr)
-    cb(err)
-  }
-}
